13c2d5ff034f64c09ae8f9f347814ab2a7f72847
Eslam-Amr <eslamamr537@gmail.com>
2026-01-23 22:24:50 +0200

    add code coverage in config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
       reportsDirectory: './coverage',
+enabled: true,
       exclude: [
         'node_modules/',
         'src/test/',
